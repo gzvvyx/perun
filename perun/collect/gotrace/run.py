67c66f3e6c283bd9a5efabacad098edd6be46701
Andrej Nespor <nespora@protonmail.com>
2024-05-06 18:39:49 +0200

    Fix overhead calculation

--- perun/collect/gotrace/run.py
+++ perun/collect/gotrace/run.py
@@ -132,8 +132,8 @@ def collect(**kwargs: Any) -> tuple[CollectStatus.OK, str, dict[str, Any]]:
             non_profiled_time = interpret.get_elapsed_time(cmderr)
 
             log.minor_success(f"real time of {str(kwargs['executable'])} alone in [s]", non_profiled_time)
-            overhead = (profiled_time - non_profiled_time) / non_profiled_time
-            log.minor_info(f"overhead x{'{:.2f}'.format(overhead)}")
+            overhead = (profiled_time) / non_profiled_time
+            log.minor_info(f"overhead {'{:.2f}'.format(overhead)}x")
 
     return CollectStatus.OK, "", dict(kwargs)
 
